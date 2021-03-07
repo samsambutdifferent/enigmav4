565230bf93dcb11d895906ee5ab941e81b66a244
samsambutdifferent <sam@solutions.to>
2021-03-07 22:28:25 +0000

    code decipher update

--- code_decrypting.py
+++ code_decrypting.py
@@ -1,5 +1,7 @@
-from enigma import Enigma
 import itertools as it
+import string
+from enigma import Enigma
+from rotor_settings import rotor_settings
 
 all_rotors = ["Beta", "Gamma", "I", "II", "III", "IV", "V"]
 even_rotors = ["Beta", "Gamma", "II", "IV"]
@@ -16,6 +18,7 @@ def __check_crib(cribs, value):
 
 def decrypter(msg, cribs, possiblities):
 
+    count = 0
     for p in possiblities:
 
         en = Enigma(
@@ -33,7 +36,12 @@ def decrypter(msg, cribs, possiblities):
 
         if ans != set():
             break
-     
+        
+        count += 1
+        if count % 1000 == 0:
+            print(f"has done {count} attempts so far, please be patient....")
+
+
     return decrypted,ans
 
 
@@ -138,36 +146,176 @@ def code_three():
     return decrypted, ans[2], ans[4]
 
 
+def code_four():
+    # Code 4
+
+    # On my way home from working late as I walked past the computer science lab I saw one of the tutors 
+    # playing with the Enigma machine. Mere tutors are not allowed to touch such important equipment! 
+    # Suspicious, I open the door, but the tutor hears me, and jumps out of the nearest window. They left behind a coded message,
+    # but some leads have been pulled out of the machine. It might contain a clue, but I'll have to find the missing 
+    # lead positions (marked with question marks in the settings below).
+
+    #     Code: SDNTVTPHRBNWTLMZTQKZGADDQYPFNHBPNHCQGBGMZPZLUAVGDQVYRBFYYEIXQWVTHXGNW
+    #     Crib: TUTOR
+
+    #     Rotors: V III IV
+    #     Reflector: A
+    #     Ring settings: 24 1   2 10
+    #     Starting positions: SWU
+    #     Plugboard pairs: WP RJ A? VF I? HN CG BS
+
+
+    code = "SDNTVTPHRBNWTLMZTQKZGADDQYPFNHBPNHCQGBGMZPZLUAVGDQVYRBFYYEIXQWVTHXGNW"
+
+    #NOTE had to update the crib as first only partially translated with orignal: Tutors
+    cribs = ["NOTUTORSWEREHARMED"]
+
+    remaining_letters = "D E K L M O Q T U X Y Z"
+
+    possible_plug_boards = []
+    orignal_string = "WP RJ A1 VF I2 HN CG BS"
+    for a_letter in remaining_letters.split():
+        string_a = orignal_string.replace("1", a_letter)
+        
+        i_remaining_letters = [x for x in remaining_letters.split() if x != a_letter]
+
+        for i_letter in i_remaining_letters:
+             string_i = string_a
+             string_i = string_i.replace("2", i_letter)
+             possible_plug_boards.append(string_i)
+
+    possiblities = list(it.product(
+        possible_plug_boards, # index 0
+        ["A"], # index 1
+        [("V", "III", "IV")], # index 2
+        [("S", "W", "U")], # index 3
+        [(24, 12, 10)], # index 4
+    ))
+
+    decrypted,ans = decrypter(msg=code, cribs=cribs, possiblities=possiblities)
+    return decrypted, ans[0]
+
+
+def code_five():
+    # Code 5¶
+
+    # I later remembered that I had given the tutor permission to use the Enigma machine to solve some codes I'd received via
+    # email. As for the window, they are just a big fan of parkour, this is always how they leave the building. It seems they
+    # are stuck on one last code. It came in via email so we suspect it's just spam, probably related to a social media website,
+    # but you never know when you'll find a gem in that kind of stuff.
+    # The tutor has narrowed the search and found most of the settings, but it seems this code was made with a non-standard
+    # reflector. Indeed, there was a photo attached to the email along with the code. It appears that the sender has taken a
+    # standard reflector, cracked it open, and swapped some of the wires – two pairs of wires have been modified, by the looks
+    # of the dodgy soldering job.
+    # To be clear, a single wire connects two letters, e.g. mapping A to Y and Y to A. The sender has taken two wires
+    # (fours pairs of letters), e.g. A-Y and H-J, and swapped one of the ends, so one option would be H-Y and A-J. 
+    # They did this twice, so they modified eight letters total (they did not swap the same wire more than once).
+    # In your answer, include what the original reflector was and the modifications.
+
+    #     Code: HWREISXLGTTBYVXRCWWJAKZDTVZWKBDJPVQYNEQIOTIFX
+    #     Crib: the name of a social media website/platform
+
+    #     Rotors: V II IV
+    #     Reflector: Unknown and non-standard (see above)
+    #     Ring settings: 06 18 07
+    #     Starting positions: AJL
+    #     Plugboard pairs: UG IE PO NX WT
+
+    code = "HWREISXLGTTBYVXRCWWJAKZDTVZWKBDJPVQYNEQIOTIFX"
+    cribs = ["FACEBOOK", "INSTAGRAM", "TWITTER", "WEIBO", "YOUTUBE"]
+
+    # take four letters and swap them to every possibe posistion
+    caps_alpha = list(string.ascii_uppercase)
+    four_char_comboz = list(it.combinations(caps_alpha,4))
+
+    reflectors = []
+    for reflector_label in all_reflectors:
+        reflectors.append(rotor_settings[reflector_label]["settings"])
+
+    # test B
+    reflector_B = rotor_settings["B"]["settings"]
+    reflector_B_variations = []
+
+    for combo in four_char_comboz:
+        new_reflector = list(reflector_B)
+        i0 = reflector_B.index(combo[0])
+        i1 = reflector_B.index(combo[1])
+        i2 = reflector_B.index(combo[2])
+        i3 = reflector_B.index(combo[3])
+
+        new_reflector[i0], new_reflector[i2] = new_reflector[i1], new_reflector[i3]
+        s = ""
+        for x in new_reflector:
+            s += x
+        reflector_B_variations.append(s)
+
+    possiblities = list(it.product(
+        ["UG IE PO NX WT"], # index 0
+        reflector_B_variations, # index 1
+        [("V", "II", "IV")], # index 2
+        [("A", "J", "L")], # index 3
+        [(6, 18, 7)], # index 4
+    ))    
+
+    ans=set()
+    count = 0
+    for p in possiblities:
+
+        en = Enigma(
+            plug_leads=p[0], 
+            reflector="A",
+            rotor_labels=p[2],
+            starting_positions=p[3],
+            ring_settings=p[4])
+
+        en.rotor_board.reflector._Rotor__contacts = p[1]
+        decrypted = en.encode_message(code)
+
+        ans = set()
+        if __check_crib(cribs, decrypted):
+            ans = (p[0], p[1], p[2], p[3], p[4])
+
+        count += 1
+        if count % 1000 == 0:
+            print(f"count: {count}")
+
+        if ans != set():
+            break 
+
+    return decrypted, ans[0]
+
+
 if __name__ == "__main__": 
 
     # # code 1
-    decrypted_msg, code_one_ans = code_one()
-    print(f"Code 1 decrypted message: {decrypted_msg}")
-    print(f"Answer to question one, missing reflector: {code_one_ans}")
+    # decrypted_msg, code_one_ans = code_one()
+    # print(f"Code 1 decrypted message: {decrypted_msg}")
+    # print(f"Answer to question one, missing reflector: {code_one_ans}")
 
-    # # code 2
-    decrypted_msg, code_two_ans = code_two()
-    print(f"Code 2 decrypted message: {decrypted_msg}")
-    print(f"Answer to question two, missing starting positions: {code_two_ans}")
+    # # # code 2
+    # decrypted_msg, code_two_ans = code_two()
+    # print(f"Code 2 decrypted message: {decrypted_msg}")
+    # print(f"Answer to question two, missing starting positions: {code_two_ans}")
 
     # code 3
-    decrypted_msg, code_three_ans_rotors, code_three_ans_ring_settings = code_three()
-    print(f"Code 3 decrypted message: {decrypted_msg}")
-    print(f"Answer to question three, missing rotors: {code_three_ans_rotors}, missing ring settings: {code_three_ans_ring_settings}")
+    # decrypted_msg, code_three_ans_rotors, code_three_ans_ring_settings = code_three()
+    # print(f"Code 3 decrypted message: {decrypted_msg}")
+    # print(f"Answer to question three, missing rotors: {code_three_ans_rotors}, missing ring settings: {code_three_ans_ring_settings}")
+
+    # Code 3 decrypted message: SQUIRRELSPLANTTHOUSANDSOFNEWTREESEACHYEARBYMERELYFORGETTINGWHERETHEYPUTTHEIRACORNS
+    # Answer to question three, missing rotors: ('II', 'Gamma',  'IV'), missing ring settings: (24, 8, 20)
+
+    # # code 4
+    # decrypted_msg, code_four_ans = code_four()
+    # print(f"Code 4 decrypted message: {decrypted_msg}")
+    # print(f"Answer to question four, plug leads: {code_four_ans}")
+    # Code 4 decrypted message: NOTUTORSWEREHARMEDNORIMPLICATEDOFCRIMESDURINGTHEMAKINGOFTHESEEXAMPLES
+    # Answer to question four, plug leads: WP RJ AT VF IK HN CG BS
 
 
-
-
-
-
-
-    # possiblities list(it.product(
-    #     list(it.product(all_rotors,all_rotors,all_rotors)), # index 0
-    #     list(it.product(all_reflectors)), # index 1
-    #     list(it.product(all_ring_settings,all_ring_settings,all_ring_settings)), # index 2
-    #     list(it.product(all_ring_settings, all_ring_settings, all_ring_settings)), # index 3
-    #     list(it.product(all_starting_pos, all_starting_pos, all_starting_pos)) # index 4
-    # )
-
+    # code 5
+    decrypted_msg, code_five_ans = code_five()
+    print(f"Code 5 decrypted message: {code_five_ans}")
+    print(f"Answer to question five, plug leads: {code_five_ans}")
 
     pass
