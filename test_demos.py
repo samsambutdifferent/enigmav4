ec072f17ed9901511ed7a95be487cbe79af18e89
samsambutdifferent <sam@solutions.to>
2021-03-07 16:04:58 +0000

    update rotor tests

--- test_demos.py
+++ test_demos.py
@@ -3,6 +3,7 @@
 """
 
 from enigma import Enigma
+from rotor import Rotor
 import pytest
 
 class TestDemos:
@@ -115,5 +116,108 @@ class TestDemos:
     
         ans = en.encode_message("BUPXWJCDPFASXBDHLBBIBSRNWCSZXQOLBNXYAXVHOGCUUIBCVMPUZYUUKHI")
         assert ans == "CONGRATULATIONSONPRODUCINGYOURWORKINGENIGMAMACHINESIMULATOR"
+    
+
+    def test_rotor(self):
+        r = Rotor("I")
+        r.status()
+        assert(r.encode_right_to_left("A") == "E")
+        assert(r.encode_left_to_right("A") == "U")
+
+        r = Rotor("III", starting_position="Z")
+        assert r.encode_right_to_left("A") == "P"
+        assert r.encode_left_to_right("A") == "N"
+
+
+        r = Rotor("III", ring_setting=2)
+        assert r.encode_right_to_left("A") == "P"
+
+
+        r = Rotor("III", ring_setting=3)
+        assert r.encode_right_to_left("A") == "S"
+        assert r.encode_left_to_right("A") == "Q"
+
+
+        r = Rotor(label="III", starting_position="B", ring_setting=26)
+        assert r.encode_right_to_left("L") == "L"
+        r = Rotor(label="III", starting_position="C", ring_setting=26)
+        assert r.encode_right_to_left("O") == "T"
+        r = Rotor(label="III", starting_position="D", ring_setting=26)
+        assert r.encode_right_to_left("V") == "K"
+        r = Rotor(label="III", starting_position="E", ring_setting=26)
+        assert r.encode_right_to_left("E") == "O"
+        assert r.encode_left_to_right("P") == "R"
+        r = Rotor(label="III", starting_position="F", ring_setting=26)
+        assert r.encode_right_to_left("I") == "S"
+        assert r.encode_left_to_right("S") == "I"
+        r = Rotor(label="III", starting_position="G", ring_setting=26)
+        assert r.encode_right_to_left("S") == "H"
+        assert r.encode_left_to_right("Y") == "V"
+        r = Rotor(label="III", starting_position="H", ring_setting=26)
+        assert r.encode_right_to_left("C") == "P"
+        assert r.encode_left_to_right("O") == "J"
+        r = Rotor(label="III", starting_position="I", ring_setting=26)
+        assert r.encode_right_to_left("O") == "J"
+        assert r.encode_left_to_right("D") == "M"
+        r = Rotor(label="III", starting_position="I", ring_setting=26)
+        assert r.encode_right_to_left("O") == "J"
+        assert r.encode_left_to_right("D") == "M"
+        r = Rotor(label="III", starting_position="J", ring_setting=26)
+        assert r.encode_right_to_left("P") == "E"
+        assert r.encode_left_to_right("C") == "L"
+        r = Rotor(label="III", starting_position="K", ring_setting=26)
+        assert r.encode_right_to_left("U") == "A"
+        assert r.encode_left_to_right("L") == "G"
+        r = Rotor(label="III", starting_position="L", ring_setting=26)
+        assert r.encode_right_to_left("V") == "D"
+        assert r.encode_left_to_right("J") == "Z"
+        r = Rotor(label="III", starting_position="M", ring_setting=26)
+        assert r.encode_right_to_left("Q") == "U"
+        assert r.encode_left_to_right("R") == "C"
+        r = Rotor(label="III", starting_position="N", ring_setting=26)
+        assert r.encode_right_to_left("B") == "Q"
+        assert r.encode_left_to_right("S") == "E"
+        r = Rotor(label="III", starting_position="O", ring_setting=26)
+        assert r.encode_right_to_left("X") == "K"
+        assert r.encode_left_to_right("B") == "J"
+        r = Rotor(label="III", starting_position="P", ring_setting=26)
+        assert r.encode_right_to_left("A") == "S"
+        assert r.encode_left_to_right("Q") == "C"
+        r = Rotor(label="III", starting_position="Q", ring_setting=26)
+        assert r.encode_right_to_left("Q") == "Y"
+        assert r.encode_left_to_right("H") == "X"
+        r = Rotor(label="III", starting_position="R", ring_setting=26)
+        assert r.encode_right_to_left("U") == "H"
+        assert r.encode_left_to_right("Y") == "G"
+        r = Rotor(label="III", starting_position="S", ring_setting=26)
+        assert r.encode_right_to_left("F") == "X"
+        assert r.encode_left_to_right("N") == "Z"
+        r = Rotor(label="III", starting_position="T", ring_setting=26)
+        assert r.encode_right_to_left("J") == "N"
+        assert r.encode_left_to_right("X") == "O"
+        r = Rotor(label="III", starting_position="U", ring_setting=26)
+        assert r.encode_right_to_left("Q") == "A"
+        assert r.encode_left_to_right("L") == "X"
+        r = Rotor(label="III", starting_position="V", ring_setting=26)
+        assert r.encode_right_to_left("W") == "K"
+        assert r.encode_left_to_right("B") == "O"
+        r = Rotor(label="III", starting_position="W", ring_setting=26)
+        assert r.encode_right_to_left("F") == "I"
+        assert r.encode_left_to_right("E") == "D"
+        r = Rotor(label="III", starting_position="X", ring_setting=26)
+        assert r.encode_right_to_left("T") == "Y"
+        assert r.encode_left_to_right("M") == "W"
+        r = Rotor(label="III", starting_position="X", ring_setting=26)
+        assert r.encode_right_to_left("T") == "Y"
+        assert r.encode_left_to_right("M") == "W"
+        r = Rotor(label="III", starting_position="Y", ring_setting=26)
+        assert r.encode_right_to_left("V") == "L"
+        assert r.encode_left_to_right("G") == "D"
+        r = Rotor(label="III", starting_position="Z", ring_setting=26)
+        assert r.encode_right_to_left("D") == "H"
+        assert r.encode_left_to_right("U") == "W"
+        r = Rotor(label="III", starting_position="A", ring_setting=26)
+        assert r.encode_right_to_left("R") == "F"
+        assert r.encode_left_to_right("Z") == "S"
 
         
